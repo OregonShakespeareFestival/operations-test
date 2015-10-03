3601d83de225bfc6d400bd0d262217b4ba2827ab
RobertLuaders <robert@luaders.com>
2015-10-03 15:14:42 -0700

    commented code

--- reconcile.py
+++ reconcile.py
@@ -15,7 +15,19 @@ f = open("logs.txt", 'w')
 #Use database connection to iterate.
 with con:
     cur = con.cursor()
+
+    #Drops temp table if exists.
     cur.execute("DROP TABLE IF EXISTS temp;")
-    cur.execute("CREATE TABLE temp (idnum INTEGER, givenName, sn, username, mail);")
-    cur.execute("CREATE INDEX s_index on temp (idnum);")
-    cur.execute("CREATE INDEX IF NOT EXISTS e_index on employees (idnum);")
+
+    #Needs to correlate index numbers.  Revisit as needed.
+    cur.execute("CREATE TABLE temp (idnum INTEGER, givenName, sn, username, mail);") #Create table for sorting
+    cur.execute("CREATE INDEX s_index on temp (idnum);") #Create index based on id_number
+    cur.execute("CREATE INDEX IF NOT EXISTS e_index on employees (idnum);") #Create an index (dictionary) for fast sort in employee table
+
+    with open('input.csv','rb') as fin:
+        dr = csv.DictReader(fin, fieldnames=("idnum", "givenName", "sn", "username", "mail")) # comma is default delimiter
+        to_db = [(i["idnum"], i["givenName"], i["sn"], i["username"], i["mail"]) for i in dr] #create array to send to database
+
+    #Insert the records to the temp table from the CSV
+    cur.executemany("INSERT INTO temp (idnum, givenName, sn, username, mail) VALUES (?, ?, ?, ?, ?);", to_db)
+    con.commit() #Clost transacation
