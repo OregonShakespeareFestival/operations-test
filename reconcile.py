b9f4f2221c82e9b9c5e318a356926654916a81ab
RobertLuaders <robert@luaders.com>
2015-10-03 15:05:05 -0700

    now opens database and text file handle

--- reconcile.py
+++ reconcile.py
@@ -5,3 +5,12 @@
 import sqlite3 as lite
 import csv
 
+
+#Create database connection
+con = lite.connect('employees.db')
+
+#Create file handle for logging and open.
+f = open("logs.txt", 'w')
+
+#Use database connection to iterate.
+with con:
