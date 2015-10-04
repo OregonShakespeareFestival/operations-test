d1dc12e420f69a1663c8ff6dd817ecae4a2c73bc
RobertLuaders <robert@luaders.com>
2015-10-04 11:39:30 -0700

    1) Renamed variables to explicit and meaningful names per *article: https://signalvnoise.com/posts/3250-clarity-over-brevity-in-variable-and-method-names 2) Cleaned up comments. * David Hansson - Creator of Ruby on Rails: "But the silliness quickly dissipates the first time you return to a piece of code and know exactly what it does."

--- reconcile.py
+++ reconcile.py
@@ -2,77 +2,77 @@
 
 # -*- coding: utf-8 -*-
 
-import sqlite3 as lite
+import sqlite3
 import csv
 
 
-#Create database connection
-con = lite.connect('employees.db')
+#Create database connection.
+dbConnect = sqlite3.connect('employees.db')
 
 #Create file handle for logging and open.
-f = open("logs.txt", 'w')
+logFile = open("logs.txt", 'w')
 
 #Use database connection to iterate.
-with con:
-    cur = con.cursor()
+with dbConnect:
+    cursor = dbConnect.cursor()
 
     #Drops temp table if exists.
-    cur.execute("DROP TABLE IF EXISTS temp;")
+    cursor.execute("DROP TABLE IF EXISTS temp;")
 
     #Needs to correlate index numbers.  Revisit as needed.
-    cur.execute("CREATE TABLE temp (idnum INTEGER, givenName, sn, username, mail);") #Create table for sorting
-    cur.execute("CREATE INDEX s_index on temp (idnum);") #Create index based on id_number
-    cur.execute("CREATE INDEX IF NOT EXISTS e_index on employees (idnum);") #Create an index (dictionary) for fast sort in employee table
+    cursor.execute("CREATE TABLE temp (idnum INTEGER, givenName, sn, username, mail);") #Create table for sorting.
+    cursor.execute("CREATE INDEX s_index on temp (idnum);") #Create index based on id_number.
+    cursor.execute("CREATE INDEX IF NOT EXISTS e_index on employees (idnum);") #Create an index (dictionary) for fast sort in employee table.
 
-    with open('input.csv','rb') as fin:
-        dr = csv.DictReader(fin, fieldnames=("idnum", "givenName", "sn", "username", "mail")) # comma is default delimiter
-        to_db = [(i["idnum"], i["givenName"], i["sn"], i["username"], i["mail"]) for i in dr] #create assoc-array to send to database
+    with open('input.csv','rb') as employeeFile:
+        dictReader = csv.DictReader(employeeFile, fieldnames=("idnum", "givenName", "sn", "username", "mail")) #Comma is default delimiter.
+        tempEmployeeCollection = [(i["idnum"], i["givenName"], i["sn"], i["username"], i["mail"]) for i in dictReader] #Create array to send to database.
 
-    #Insert the records to the temp table from the CSV
-    cur.executemany("INSERT INTO temp (idnum, givenName, sn, username, mail) VALUES (?, ?, ?, ?, ?);", to_db)
-    con.commit() #Close transacation
+    #Insert the records to the temp table from the CSV.
+    cursor.executemany("INSERT INTO temp (idnum, givenName, sn, username, mail) VALUES (?, ?, ?, ?, ?);", tempEmployeeCollection)
+    dbConnect.commit() #Close transacation.
 
-    #Return all employee records from SQLite to cur obj.
-    cur.execute("SELECT * FROM employees")
+    #Return all employee records from SQLite3 to cursor object.
+    cursor.execute("SELECT * FROM employees")
 
     #Create rows object to iterate over.
-    rows = cur.fetchall() #Return all records from the employee table
+    rows = cursor.fetchall() # Return all records from the employee table.
 
     for row in rows:
-        cur.execute("SELECT * FROM temp WHERE idnum = " + str(row[0]) + ";") #Comparing idnum to string, returning mathing row
+        cursor.execute("SELECT * FROM temp WHERE idnum = " + str(row[0]) + ";") #Comparing idnum to string. Return mathing row.
         try:
-            cur.fetchall()[0] # Try to return pk
+            cursor.fetchall()[0] #Try to return primary key.
         except:
-            cur.execute("DELETE from employees WHERE idnum = " + str(row[0]) + ";") #If no match, generate exception drop record
-            f.write("DELETED row with idnum = " + str(row[0]) + " from employees\n") #Log deletion
-            f.flush() #Save and Close file
+            cursor.execute("DELETE from employees WHERE idnum = " + str(row[0]) + ";") #If no match, generate exception, drop record.
+            logFile.write("DELETED row with idnum = " + str(row[0]) + " from employees\n") #Log deletion.
+            logFile.flush() #Save and Close file.
 
-    con.commit() #Commits transactions to database
+    dbConnect.commit() #Commits transactions to database.
 
-    #Return all records from temp database
-    cur.execute("SELECT * FROM temp")
-    rows = cur.fetchall()
+    #Return all records from temp database.
+    cursor.execute("SELECT * FROM temp")
+    rows = cursor.fetchall()
 
-    #Inserting new records
+    #Inserting new records.
     count = 0
     for row in rows:
         count += 1
         error = False
         for i in range(5):
-            if not row[i] and row[i] != 0: #Checks to see if range is not empty
-                f.write("Malformed row at line " + str(count) + " in input.csv\n")
+            if not row[i] and row[i] != 0: #Checks to see if range is not empty.
+                logFile.write("Malformed row at line " + str(count) + " in input.csv\n")
                 print "Malformed row at line " + str(count) + " in input.csv"
                 error = True
                 break
-        if not error: #If not malformed contine
-            cur.execute("SELECT * FROM employees WHERE idnum = " + str(row[0]) + ";") #attempt match on exisitng employee
+        if not error: #If not malformed continue.
+            cursor.execute("SELECT * FROM employees WHERE idnum = " + str(row[0]) + ";") #Attempt match on exisitng employee.
             try:
-                cur.fetchall()[0]
+                cursor.fetchall()[0]
             except:
-                cur.execute("INSERT INTO employees VALUES (?, ?, ?, ?, ?);", row) #Consider entry new insert record
-                f.write("INSERTED row with idnum = " + str(row[0]) + " to employees\n")
-                f.flush()
+                cursor.execute("INSERT INTO employees VALUES (?, ?, ?, ?, ?);", row) #Consider entry of new insert record.
+                logFile.write("INSERTED row with idnum = " + str(row[0]) + " to employees\n")
+                logFile.flush()
 
-    cur.execute("DROP TABLE IF EXISTS temp;") #Drop temp table
-    con.commit() #Commit any transactions to database
+    cursor.execute("DROP TABLE IF EXISTS temp;") #Drop temporary table.
+    dbConnect.commit() #Commit any transactions to database.
 
